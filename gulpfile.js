fb133e86e475e815cbe1d4fc095a1c1411b4b637
ryavin86 <ryavin_86@mail.ru>
2020-03-02 23:09:45 +0300

    include style.css make min.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
 
 gulp.task('hello', function(done) {
   console.log('Привет, мир');
@@ -14,4 +16,11 @@ gulp.task('browser-sync', function() {
       }
   });
   gulp.watch("./WebStart2/index.html").on('change', browserSync.reload);
+});
+
+gulp.task('default', function () {
+  gulp.src('./WebStart2/style.css')
+      .pipe(cssmin())
+      .pipe(rename({suffix: '.min'}))
+      .pipe(gulp.dest('dist'));
 });
