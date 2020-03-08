7a63a39de6a4f9fa7bc7ad7d31e8712e274aa7f2
ryavin86 <ryavin_86@mail.ru>
2020-03-09 00:11:17 +0300

    feat: add Futura Book and Demi fonts

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var {src, dest, watch} = require('gulp');
 var browserSync = require('browser-sync').create();
 var rename = require('gulp-rename');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 // Static server
 function bs() {
@@ -13,14 +14,17 @@ function bs() {
   });
   watch("index.html").on('change', browserSync.reload);
   watch("sass/**/*.sass").on('change', serveSass);
+  watch("sass/**/*.scss").on('change', serveSass);
   watch("js/*.js").on('change', browserSync.reload);
 }
 
 function serveSass() {
-  return src("sass/**/*.sass")
+  return src("sass/**/*.sass", "sass/**/*.scss")
       .pipe(sass())
+      .pipe(autoprefixer({
+        cascade: false}))
       .pipe(dest("css"))
-      .pipe(browserSync.stream());
+      .pipe(browserSync.stream())
 };
 
 exports.serve = bs;
