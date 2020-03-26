c4e9f9405aeccabf2896b214dc89f880087317cb
ryavin86 <ryavin_86@mail.ru>
2020-03-26 16:10:12 +0300

    feat: add send form in email

---
+++ send.php
@@ -0,0 +1,42 @@
+<?php
+
+$userName = $_POST['userName'];
+$userEmail = $_POST['userEmail'];
+$userPhone = $_POST['userPhone']
+
+
+// Load Composer's autoloader
+require 'phpmailer/Exception.php';
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.mail.ru';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'ryavin_86@mail.ru';                     // SMTP username
+    $mail->Password   = 'Forza_Italia1986';                               // SMTP password
+    $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    //Recipients
+    $mail->setFrom('ryavin_86@mail.ru', 'Виктор');
+    $mail->addAddress('ryabish@gmail.com', 'Joe User');     // Add a recipient
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Новая заявка с сайта';
+    $mail->Body    = "Имя пользователя: ${userName}, его телефон: ${userPhone}, email: ${userEmail} ";
+
+    $mail->send();
+    header('Location: thanks.html')
+} catch (Exception $e) {
+    echo "Письмо не отпрвлено. Mailer Error: {$mail->ErrorInfo}";
+}
+
+?>
